d945b79b15d89179249425a8f6903ac838535221
filipe-eich <filipe.eich@gmail.com>
2025-02-25 20:43:08 -0300

    Add files via upload

---
+++ calc_soma.py
@@ -0,0 +1,30 @@
+"""
+Programa: calc_soma
+Descrição: Soma 2 números à escolha do usuário
+Autor: Filipe Eich
+Data: 25/02/2025
+Versao: 0.0.1
+
+"""
+
+#Alocaçao de memoria
+
+var1=""
+var2=""
+
+#Entrada de dados
+
+var1 = input("\nOlá! Vamos somar dois números? Digite o primeiro número de sua escolha: ")
+
+var2 = input("\nPerfeito, registrado. Agora digite o segundo número de sua escolha: ")
+
+
+# Processamento de dados
+
+soma=var1+var2
+
+
+#Saida de dados
+
+print(f"\nA soma dos números é: {soma}")
+
